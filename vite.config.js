54693e8896f27f897684d3f3300a0d4e3d18c927
babarnawaz <nawazbabar398@gmailcom>
2026-08-17 15:08:49 +0500

    Update Config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/paradise-nursery/", // Must match repository name exactly
 });
